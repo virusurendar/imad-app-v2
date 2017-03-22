61e7acc972d73df5af62bb5f76738a4ee40412c2
virusurendar <virusurendar@gmail.com>
2017-03-22 17:03:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/', function (req, res) {
+app.get('/viru', function (req, res) {
   res.send('i am surendar');
 });
 
