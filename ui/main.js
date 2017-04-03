7c4ac9067238edd318880151f08bf17a7bef9e57
virusurendar <virusurendar@gmail.com>
2017-04-03 12:53:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,15 @@
 console.log('Loaded!');
+function validate()
+{
+    if(   document.getElementById("user").value == "swomiya"
+       && document.getElementById("password").value == "openit" )
+    {
+        alert( "validation succeeded" );
+        location.href="run.html";
+    }
+    else
+    {
+        alert( "validation failed" );
+        location.href="fail.html";
+    }
+}
